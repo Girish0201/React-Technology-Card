a63e10253ad29f782088f9ed38670c74f8747d37
Girish0212 <bathigirish@gmail.com>
2023-02-20 17:26:32 +0000

    commit message

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import TrendingTechnologies from './components/CardItem'
+import './App.css'
+
+const cardsList = [
+  {
+    id: 1,
+    title: 'Data Scientist',
+    description:
+      'Data scientists gather and analyze large sets of structured and unstructured data',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/data-scientist-img.png',
+    className: 'card-1',
+  },
+  {
+    id: 2,
+    title: 'IOT Developer',
+    description:
+      'IoT Developers are professionals who can develop, manage, and monitor IoT devices.',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/iot-developer-img.png',
+    className: 'card-2',
+  },
+  {
+    id: 3,
+    title: 'VR Developer',
+    description:
+      'A VR developer creates completely new digital environments that people can see.',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/vr-developer-img.png',
+    className: 'card-3',
+  },
+  {
+    id: 4,
+    title: 'ML Engineer',
+    description:
+      'Machine learning engineers feed data into models defined by data scientists.',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/ml-engineer-img.png',
+    className: 'card-4',
+  },
+]
+
+const App = () => (
+  <div className="technology-container">
+    <h1 className="title"> Learn 4.0 Technologies</h1>
+    <p className="text">
+      Get trained by alumni of IITs and top companies like amazon,microsoft,
+      intel,tcs,wipro,aqulcom etc.Learn directly from Profossionls involved in
+      project development
+    </p>
+    <ul className="row-container">
+      {cardsList.map(eachTechnology => (
+        <TrendingTechnologies
+          cardDetails={eachTechnology}
+          key={eachTechnology.id}
+        />
+      ))}
+    </ul>
+  </div>
+)
+
+export default App
